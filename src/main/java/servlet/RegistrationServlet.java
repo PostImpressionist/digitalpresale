b11b23c1f23b1f24c05415c6461ee1836e9ab03a
Evgeny Kandaurov <evgeny.kandaurov@gmail.com>
2021-11-19 01:06:29 +0300

    RegistrationServlet added with doGet w\o params

---
+++ src/main/java/servlet/RegistrationServlet.java
@@ -0,0 +1,20 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * created by E.K.
+ * 2021 November 19
+ **/
+
+public class RegistrationServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/registration.jsp").forward(req, resp);
+    }
+}
